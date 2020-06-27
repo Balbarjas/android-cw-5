4dd63144ce7e248db956902e24c3ecea4d44d1e2
barjas albarjas <baalbarjas@hotmail.com>
2020-06-27 19:17:11 +0300

    ...

---
+++ cw/app/src/main/java/com/example/cw/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.cw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private View.OnClickListener view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button bage2 =findViewById(R.id.button);
+        Button bage3 =findViewById(R.id.button2);
+
+        bage2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer i  = +Integer.valueOf(MainActivity2.this.MainActivity.; class);
+            }
+    });
+        bage3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Integer i  = +Integer.valueOf(MainActivity3.this.MainActivity.; class);
+            }
+        });
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
